2aa0e85c29f87da3de630d84fad0e18b7ac4df2c
kanes115 <dominikstanaszek@icloud.com>
2018-06-24 23:04:58 +0200

    Test controller and lobby channel

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -5,4 +5,21 @@ defmodule WhatIf.PageControllerTest do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
+
+  test "GET /test", %{conn: conn} do
+    conn = get conn, "/test"
+    assert json_response(conn, 200) == %{"test" => "ok"}
+  end
+
+  test "GET /games with no authorization header return 403", %{conn: conn} do
+    conn = get conn, "/games"
+    assert response(conn, 403) =~ "Unauthorized"
+  end
+
+  test "GET /game/:id with no authorization header return 403 even when :id does not exist",
+  %{conn: conn} do
+    conn = get conn, "/game/2334"
+    assert response(conn, 403) =~ "Unauthorized"
+  end
+
 end
