d3b0a0d5eee9265b4fa31cf4ac147080ec1e9617
kanes115 <dominikstanaszek@icloud.com>
2018-06-25 17:25:21 +0200

    Adjust tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,13 +1,25 @@
 defmodule WhatIf.PageControllerTest do
   use WhatIf.ConnCase
 
-  test "GET /", %{conn: conn} do
+  @token "eyJhbGciOiJSUzI1NiIsImtpZCI6IjYyMDg0NmQxNDVjN2VjNjQ0ODU5MmFjZWYzMGVhYmE1NzA4NmMwYWUifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdGFpLXdoYXQtaWYiLCJhdWQiOiJ0YWktd2hhdC1pZiIsImF1dGhfdGltZSI6MTUyOTkzOTUzOSwidXNlcl9pZCI6InZZc2s4dnk0MUtoUDVpdHFXZEYxRnd3RFRpYzIiLCJzdWIiOiJ2WXNrOHZ5NDFLaFA1aXRxV2RGMUZ3d0RUaWMyIiwiaWF0IjoxNTI5OTM5NTYyLCJleHAiOjE1Mjk5NDMxNjIsImVtYWlsIjoidGVzdF91c2VyQG5lY2Vzc2FyeWZvcnRlc3RzLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ0ZXN0X3VzZXJAbmVjZXNzYXJ5Zm9ydGVzdHMuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.RgJEgOvu7T7ivxUnp83i9xzVyouzo2dbUWZFXuhcL-Y8BqisZn_QkhFoVZrd-Ji1iNPihXSJ4_46jUMEp0bkLfwoz98mBFa8G87zLO_50dI5yuFe1JWkKU8GX-Jo_KU9duhsIcgWsynCGfpFdhHtT0KTEUVOjg-mDWHFTF4HTk7Ezn7IDTOB7rcsGEjr9DIBC9cJ8FDxxNebB5bHMMYEOBPhgRH1_VZqUk-gsWa0H-zzA9Pw_u6gsibFM_3UKet9eIVAIFvvI4TwSqIFc--JY1mVpHAWDHDoYpBQBekL3y9YMhDB7UwQWHn9_oYumQfobTH0Ktv29L8dlvWZ0ADfEw"
+
+  test "GET / returns 403 if token is not provided", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert response(conn, 403) =~ "Unauthorized"
   end
 
-  test "GET /test", %{conn: conn} do
-    conn = get conn, "/test"
+
+  test "GET / returns 200 with auth header", %{conn: _conn} do
+    conn = build_conn()
+    |> put_token_header
+    |> get("/")
+    assert html_response(conn, 200) =~ "Welcome"
+  end
+
+  test "GET /test", %{conn: _conn} do
+    conn = build_conn()
+           |> put_token_header
+           |> get("/test")
     assert json_response(conn, 200) == %{"test" => "ok"}
   end
 
@@ -20,6 +32,11 @@ defmodule WhatIf.PageControllerTest do
   %{conn: conn} do
     conn = get conn, "/game/2334"
     assert response(conn, 403) =~ "Unauthorized"
+  end
+
+  defp put_token_header(conn) do
+    conn
+    |> put_req_header("authorization", "Bearer #{@token}")
   end
 
 end
