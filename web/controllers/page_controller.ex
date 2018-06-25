bdf432205c42d1cbf058d5e8b5f4be9c92b9325b
kanes115 <dominikstanaszek@icloud.com>
2018-06-25 09:31:45 +0200

    Parse to json

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,49 @@
 defmodule WhatIf.PageController do
   use WhatIf.Web, :controller
 
+  defmodule TermParser do
+    def parse(str) when is_binary(str)do
+      case str |> Code.string_to_quoted do
+        {:ok, terms} -> hydrate_terms(terms)
+        {:error, err} -> {:error, err}
+      end
+    end
+
+    defp hydrate_terms(terms) do
+      try do
+        {:ok, _parse(terms)}
+      rescue
+        e in ArgumentError -> {:error, e}
+      end
+    end
+
+    # atomic terms
+    defp _parse(term) when is_atom(term), do: term
+    defp _parse(term) when is_integer(term), do: term
+    defp _parse(term) when is_float(term), do: term
+    defp _parse(term) when is_binary(term), do: term
+
+    defp _parse([]), do: []
+    defp _parse([h|t]), do: [_parse(h) | _parse(t)]
+
+    defp _parse({a, b}), do: {_parse(a), _parse(b)}
+    defp _parse({:"{}", _place, terms}) do
+      terms
+      |> Enum.map(&_parse/1)
+      |> List.to_tuple
+    end
+
+    defp _parse({:"%{}", _place, terms}) do
+      for {k, v} <- terms, into: %{}, do: {_parse(k), _parse(v)}
+    end
+
+    defp _parse(_) do
+      raise ArgumentError, message: "string contains non-literal term(s)"
+    end
+
+  end
+
+
   def index(conn, _params) do
     render conn, "index.html"
   end
@@ -11,12 +54,12 @@ defmodule WhatIf.PageController do
 
   def set_display_name(conn, %{"name" => name}) do
     fun = fn user_id ->
-        register(user_id, name, conn)
-        {201, "ok"}
+      register(user_id, name, conn)
+      {201, "ok"}
     end
     maybe_do(conn, fun)
   end
-  
+
   def register(user_id, name, conn) do
     changeset = 
       WhatIf.User.registration_changeset(%WhatIf.User{}, %{display_name: name, user_id: user_id})
@@ -33,7 +76,7 @@ defmodule WhatIf.PageController do
       games = WhatIf.Game.get_user_games_list(user_id)
               |> Enum.map(fn {id, name, date} ->
                 %{"id" => id, "room_name" => name, "date" => date} end)
-        {200, %{"games" => games}}
+                {200, %{"games" => games}}
     end
     maybe_do(conn, fun)
   end
@@ -47,7 +90,8 @@ defmodule WhatIf.PageController do
         _ ->
           users = game.users
                   |> Enum.map(fn %{display_name: name} -> name end)
-          {200, %{"q_and_a" => game.questions, "players" => users}}
+          {:ok, json_q} = TermParser.parse(game.questions)
+          {200, %{"q_and_a" => json_q, "players" => users}}
       end
     end
     maybe_do(conn, fun)
@@ -74,4 +118,5 @@ defmodule WhatIf.PageController do
     end
   end
 
+  
 end
